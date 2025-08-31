c15c923de932ce55179f2ce21d8f6b9b2b39f2d5
Andjela Maskovic <andjela.maskovic@b-one.me>
2025-08-31 13:36:50 +0200

    displaying user profile

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,7 @@ import { Amplify } from "aws-amplify";
 import "@aws-amplify/ui-react/styles.css";
 import { generateClient } from "aws-amplify/data";
 import outputs from "../amplify_outputs.json";
-/**
- * @type {import('aws-amplify/data').Client<import('../amplify/data/
- resource').Schema>}
- */
+
 Amplify.configure(outputs);
 const client = generateClient({
     authMode: "userPool",
